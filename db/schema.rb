eee0d773fc481173801a9746c86b69e9ca58d3e7
Garrett Wesley <garrettwesley@berkeley.edu>
2017-11-09 00:23:34 -0800

    Add associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109081059) do
+ActiveRecord::Schema.define(version: 20171109082206) do
 
   create_table "postings", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20171109081059) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_postings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
