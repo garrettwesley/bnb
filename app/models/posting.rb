e4504eb08a8f4e07682b06109a9e6eb1786d9587
Garrett Wesley <garrettwesley@berkeley.edu>
2017-11-15 17:28:15 -0800

    Update to master

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,3 +1,4 @@
 class Posting < ApplicationRecord
   belongs_to :user
+  validates :start, :end, :leaving, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :postings
+  get 'users/postings', to: 'users#show', as: 'show_user_posts'
   root to: 'postings#index'
 end
