317406e6ada88c8e9033e1004bece83733c2ab8d
Garrett Wesley <garrettwesley@berkeley.edu>
2017-11-28 01:51:46 -0800

    Added messaging and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :postings
+  
+  resources :conversations do
+    resources :messages
+  end
+
   get 'users/postings', to: 'users#show', as: 'show_user_posts'
   root to: 'postings#index'
 end
